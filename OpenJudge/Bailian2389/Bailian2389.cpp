98e2888f012b9ad8d80721a695a706c2796233fd
mobileliker <mobileliker@gmail.com>
2014-11-13 17:19:05 +0800

    Finish OpenJudge Bailian 2389

---
+++ OpenJudge/Bailian2389/Bailian2389.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string mul(string str1,string str2)
+{
+    vector<int> v_res(str1.size()+str2.size(),0);
+    string::size_type i,j;
+    vector<int>::size_type k,p;
+
+    reverse(str1.begin(),str1.end());
+    reverse(str2.begin(),str2.end());
+    for(i = 0; i != str1.size(); ++i)
+    {
+        for(j = 0; j != str2.size(); ++j)
+        {
+            v_res[i+j] += (str1[i]-'0') * (str2[j] - '0');
+        }
+    }
+    for(k = 0; k != v_res.size() - 1; ++k)
+    {
+        v_res[k+1] += v_res[k] / 10;
+        v_res[k] = v_res[k] % 10;
+    }
+
+    for(p = v_res.size() - 1; p != -1; --p)
+    {
+        if(v_res[p] != 0) break;
+    }
+    if(p == -1) p = 0;
+
+    string s_res(p+1,'0');
+    for(k = p; k != -1; --k) s_res[p-k] = char(v_res[k] + '0');
+    
+
+    return s_res;
+
+}
+
+int main()
+{
+    //freopen("D://input.txt","r",stdin);
+    
+    string str1,str2;
+    cin >> str1 >> str2;
+
+    cout << mul(str1,str2) << endl;
+
+    return 0;
+}
