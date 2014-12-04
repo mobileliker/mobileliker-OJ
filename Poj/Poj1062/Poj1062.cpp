0654d88c8aabc78ccc0940b84768518fa99fb3a3
mobileliker <mobileliker@gmail.com>
2014-12-04 16:23:50 +0800

    Finish Poj 1062
    http://www.cnblogs.com/mobileliker/p/4143127.html

--- Poj/Poj1062/Poj1062.cpp
+++ Poj/Poj1062/Poj1062.cpp
@@ -1,156 +1,149 @@
 #include <iostream>
 #include <cstring>
-#include <cstdlib>
 #include <limits>
+#include <cstdlib>
+#include <cmath>
 
 using namespace std;
 
-const int intMax = numeric_limits<int>::max();
+int intMax = numeric_limits<int>::max();
 
 int main()
 {
 	//freopen("D://input.txt","r",stdin);
+	int i,j, k, ii;
 
-	int i,j,k,LL;
-
-	int m,n;
+	int m, n;
 	cin >> m >> n;
-
-	//cout << "m=" << m << endl;
-	//cout << "n=" << n << endl;
+	//cout << m << " " << n << endl;
 
 	int **arr_map = new int*[n];
-	for(j = 0; j < n; ++j)
-	{
-		arr_map[j] = new int[n];
-		//memset(arr_map[j],0,sizeof(int) * n);
-	}
 	for(i = 0; i < n; ++i)
 	{
+		arr_map[i] = new int[n];
 		for(j = 0; j < n; ++j) arr_map[i][j] = intMax;
 	}
 
-	int *arr_L = new int[n];
 	int *arr_p = new int[n];
+	int *arr_L = new int[n];
 
 	int p,L,x;
-	//int L0;
 	for(i = 0; i < n; ++i)
 	{
 		cin >> p >> L >> x;
-		//cout << p << " " << L << " " << x << endl;
 		arr_p[i] = p;
-		//if(0 == i) L0 = L;
 		arr_L[i] = L;
 
 		int t,v;
 		for(j = 0; j < x; ++j)
 		{
 			cin >> t >> v;
-			//cout << t << " " << v << endl;
-
-			//if(abs(L0 - L) <= m)
-			//{
-				arr_map[i][t - 1] = v;
-				arr_map[t - 1][i] = v;
-			//}
+			arr_map[t - 1][i] = v;
 		}
 	}
 
-	//for(i = 1; i < n; ++i)
+	//for(j = 0; j < n; ++j)
 	//{
-	//	if(abs(arr_L[0] - arr_L[i]) > m)
-	//	{
-	//		for(j = 0; j < n; ++j) 
-	//		{
-	//			arr_map[i][j] = intMax;
-	//			arr_map[j][i] = intMax;
-	//		}
-	//	}
-	//}
-
-	//for(i = 0; i < n; ++i)
-	//{
-	//	for(j = 0; j < n; ++j)
-	//	{
-	//		cout << arr_map[i][j] << " ";
-	//	}
+	//	for(k = 0; k < n; ++k) cout << arr_map[j][k] << " ";
 	//	cout << endl;
 	//}
-	
-	//for(i = 0; i < n; ++i) cout << arr_p[i] << " ";
 	//cout << endl;
 
-	//for(i = 0; i < n; ++i) cout << arr_L[i] << " ";
-	//cout << endl;
+	int min_p = arr_p[0];
 
 	int *arr_d = new int[n];
 	bool *arr_m = new bool[n];
 
-	int min_p = arr_p[0];
-
-	int L_top,L_bottom;
-	for(LL = 0; LL <= m; ++LL)
-	{
-	L_top = arr_L[0] + LL;
-	L_bottom = arr_L[0] - m + LL;
+	int **arr_map2 = new int*[n];
+	for(i = 0; i < n; ++i) arr_map2[i] = new int[n];
 
 	for(i = 1; i < n; ++i)
 	{
-		if(arr_L[i] > L_top || arr_L[i] < L_bottom) continue;
-
-		memset(arr_m,0,sizeof(bool) * n);
-		//memcpy(arr_d,arr_map[i],sizeof(int) * n);
-		for(j = 0; j < n; ++j)
+		for(ii = 0; ii <= m; ++ii)
 		{
-			if(arr_L[j] >= L_bottom && arr_L[j] <= L_top) arr_d[j] = arr_map[i][j];
-		}
-		arr_d[i] = 0;
-		arr_m[i] = true;
+			int l_bettom = arr_L[i] - m + ii;
+			int l_top = arr_L[i] + ii;
 
-		
-		//for(k = 0; k < n; ++k) cout << arr_d[k] << " ";
-		//cout << endl;
-
-		for(j = 1; j < n; ++j)
-		{
-			int min_d = intMax;
-			int idx_d;
-			for(k = 0; k < n; ++k)
+			for(j = 0; j < n; ++j) memcpy(arr_map2[j], arr_map[j], sizeof(int) * n);
+			for(j = 0; j < n; ++j)
 			{
-				if(!arr_m[k] && arr_L[k] >= L_bottom && arr_L[k] <= L_top && min_d > arr_d[k])
+				/*if(abs(arr_L[j] - arr_L[i]) > m)
 				{
-					min_d = arr_d[k];
-					idx_d = k;
-				}
-			}
-			if(min_d == intMax) break;
-			arr_m[idx_d] = true;
-			for(k = 0; k < n; ++k)
-			{
-				if(!arr_m[k] && arr_map[idx_d][k] != intMax && arr_L[k] >= L_bottom && arr_L[k] <= L_top && min_d + arr_map[idx_d][k] < arr_d[k])
+					for(k = 0; k < n; ++k)
+					{
+						arr_map2[k][j] = intMax;
+						arr_map2[j][k] = intMax;
+					}
+				}*/
+				if(arr_L[j] < l_bettom || arr_L[j] > l_top)
 				{
-					arr_d[k] = min_d + arr_map[idx_d][k];
+					for(k = 0; k < n; ++k)
+					{
+						arr_map2[k][j] = intMax;
+						arr_map2[j][k] = intMax;
+					}
 				}
 			}
 
-			//for(k = 0; k < n; ++k) cout << arr_d[k] << " ";
+			//for(j = 0; j < n; ++j)
+			//{
+			//	for(k = 0; k < n; ++k) cout << arr_map2[j][k] << " ";
+			//	cout << endl;
+			//}
 			//cout << endl;
+
+			memcpy(arr_d, arr_map2[i], sizeof(int) * n);
+			memset(arr_m, 0 ,sizeof(bool) * n);
+
+			arr_d[i] = 0;
+			arr_m[i] = true;
+
+			for(j = 1; j < n; ++j)
+			{
+				int min_d = intMax;
+				int idx_d;
+
+				for(k = 0; k < n; ++k)
+				{
+					if(!arr_m[k] && min_d > arr_d[k])
+					{
+						min_d = arr_d[k];
+						idx_d = k;
+					}
+				}
+				if(min_d == intMax) break;
+
+				arr_m[idx_d] = true;
+				for(k = 0; k < n; ++k)
+				{
+					if(!arr_m[k] && arr_map2[idx_d][k] != intMax && min_d + arr_map2[idx_d][k] < arr_d[k])
+					{
+						arr_d[k] = min_d + arr_map2[idx_d][k];
+					}
+				}
+
+				//for(k = 0; k < n; ++k) cout << arr_d[k] << " ";
+				//cout << endl;
+			}
+
+			if(arr_d[0] != intMax && min_p > arr_d[0] + arr_p[i]) min_p = arr_d[0] + arr_p[i];
 		}
-
-		if(arr_d[0] != intMax && min_p > arr_d[0] + arr_p[i]) min_p = arr_d[0] + arr_p[i];
-
 	}
-
-	}//LL
 
 	cout << min_p << endl;
 
-	delete [] arr_p;
-	delete [] arr_L;
+	for(i = 0; i < n; ++i) delete [] arr_map2[i];
+	delete [] arr_map2;
 
-	for(j = 0; j < n; ++j) delete [] arr_map[j];
+	delete [] arr_m;
+	delete [] arr_d;
+
+	delete [] arr_L;
+	delete [] arr_p;
+
+	for(i = 0; i < n; ++i) delete [] arr_map[i];
 	delete [] arr_map;
+
 
 	return 0;
 }
